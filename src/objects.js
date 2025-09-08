26d9978440b8ff05928982ddd9a67ef10060c591
Sara Martín <saramartinfuentes@hotmail.com>
2025-09-08 19:32:37 +0200

    project created

---
+++ src/objects.js
@@ -0,0 +1,243 @@
+export const sentences = [
+  {
+    number: 35,
+    true: "La bandera era de España.",
+    false: "La bandera era de Francia.",
+  },
+  {
+    number: 2,
+    true: "La bombilla estaba apagada.",
+    false: "La bombilla estaba rota.",
+  },
+  {
+    number: 3,
+    true: "El bolso era negro.",
+    false: "El bolso era gris.",
+  },
+  {
+    number: 4,
+    true: "El coco estaba cortado.",
+    false: "El coco estaba entero.",
+  },
+  {
+    number: 5,
+    true: "El cuaderno tenía anillas.",
+    false: "El cuaderno tenía separadores.",
+  },
+  {
+    number: 23,
+    true: "El sushi era de pescado.",
+    false: "El sushi era de verdura.",
+  },
+  {
+    number: 44,
+    true: "El zumo era de naranja.",
+    false: "El zumo era de melocotón.",
+  },
+  {
+    number: 36,
+    true: "El anillo era de oro.",
+    false: "El anillo era de plata.",
+  },
+  {
+    number: 40,
+    true: "La bufanda tenía rayas.",
+    false: "La bufanda tenía estrellas.",
+  },
+  {
+    number: 28,
+    true: "La cama era doble.",
+    false: "La cama era individual.",
+  },
+  {
+    number: 37,
+    true: "El cinturón era de cuero.",
+    false: "El cinturón era de tela.",
+  },
+  {
+    number: 33,
+    true: "El cubo tenía círculos.",
+    false: "El cubo tenía cuadros.",
+  },
+  {
+    number: 41,
+    true: "La flor era una rosa.",
+    false: "La flor era una margarita.",
+  },
+  {
+    number: 32,
+    true: "La flauta era de madera.",
+    false: "La flauta era de plástico.",
+  },
+  {
+    number: 30,
+    true: "La galleta tenía chocolate.",
+    false: "La galleta tenía nata.",
+  },
+  {
+    number: 8,
+    true: "Las gafas eran graduadas.",
+    false: "Las gafas eran de sol.",
+  },
+  {
+    number: 34,
+    true: "La salchicha tenía mostaza.",
+    false: "La salchicha tenía mayonesa.",
+  },
+  {
+    number: 31,
+    true: "El batido era de fresa.",
+    false: "El batido era de banana.",
+  },
+  {
+    number: 15,
+    true: "El pan estaba cortado.",
+    false: "El pan era redondo.",
+  },
+  {
+    number: 39,
+    true: "La hucha tenía monedas.",
+    false: "La hucha tenía billetes.",
+  },
+  {
+    number: 42,
+    true: "El pimiento era amarillo.",
+    false: "El pimiento era verde.",
+  },
+  {
+    number: 48,
+    true: "El queso tenía agujeros.",
+    false: "El queso era para untar.",
+  },
+  {
+    number: 43,
+    true: "La vela era roja.",
+    false: "La vela era morada.",
+  },
+  {
+    number: 45,
+    true: "La copa era de vino tinto.",
+    false: "La copa era de vino blanco.",
+  },
+  {
+    number: 27,
+    true: "En la pantalla del móvil aparecía un gato.",
+    false: "En la pantalla del móvil aparecía un pájaro.",
+  },
+  {
+    number: 24,
+    true: "La sudadera se abrochaba con cremallera.",
+    false: "La sudadera se abrochaba con botones.",
+  },
+  {
+    number: 46,
+    true: "Las botas eran de cordones.",
+    false: "Las botas eran de velcro.",
+  },
+  {
+    number: 26,
+    true: "El lapicero tenía lápices.",
+    false: "El lapicero tenía rotuladores.",
+  },
+  {
+    number: 20,
+    true: "La bolsa era de papel.",
+    false: "La bolsa era de plástico.",
+  },
+  {
+    number: 19,
+    true: "Los auriculares eran por cable.",
+    false: "Los auriculares eran inalámbricos.",
+  },
+  {
+    number: 16,
+    true: "La guitarra era de juguete.",
+    false: "La guitarra era clásica.",
+  },
+  {
+    number: 1,
+    true: "La cerveza era roja.",
+    false: "La cerveza era rubia.",
+  },
+  {
+    number: 47,
+    true: "La raqueta era de tenis.",
+    false: "La raqueta era de pádel.",
+  },
+  {
+    number: 21,
+    true: "La pizza tenía tomate.",
+    false: "La pizza tenía jamón.",
+  },
+  {
+    number: 18,
+    true: "La pelota era de baloncesto.",
+    false: "La pelota era de fútbol.",
+  },
+  {
+    number: 17,
+    true: "La tabla era de madera.",
+    false: "La tabla era de plástico.",
+  },
+  {
+    number: 14,
+    true: "La manguera estaba enrollada.",
+    false: "La manguera estaba conectada.",
+  },
+  {
+    number: 25,
+    true: "El sándwich llevaba huevo.",
+    false: "El sándwich llevaba anchoas.",
+  },
+  {
+    number: 29,
+    true: "Las chanclas tenían estampado de flores.",
+    false: "Las chanclas tenían estampado de coches.",
+  },
+  {
+    number: 13,
+    true: "El sofá era de dos plazas.",
+    false: "El sofá era de tres plazas.",
+  },
+  {
+    number: 22,
+    true: "La falda era blanca.",
+    false: "La falda era celeste.",
+  },
+  {
+    number: 12,
+    true: "El casco era de bicicleta.",
+    false: "El casco era de obra.",
+  },
+  {
+    number: 11,
+    true: "El microondas tenía una taza.",
+    false: "El microondas tenía un plato.",
+  },
+  {
+    number: 10,
+    true: "El cigarrillo estaba intacto.",
+    false: "El cigarrillo estaba encendido.",
+  },
+  {
+    number: 9,
+    true: "La báscula marcaba cero.",
+    false: "La báscula marcaba cien.",
+  },
+  {
+    number: 38,
+    true: "En la bañera había una toalla.",
+    false: "En la bañera había un bote de champú.",
+  },
+  {
+    number: 7,
+    true: "El hilo era azul.",
+    false: "El hilo era rojo.",
+  },
+  {
+    number: 6,
+    true: "La muñeca llevaba un vestido.",
+    false: "La muñeca llevaba unos pantalones.",
+  },
+];
+
